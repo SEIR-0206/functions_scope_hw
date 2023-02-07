adfc9b2a7a127656eb8e59f9147a61a72162f5f4
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-02-06 20:20:14 -0500

    Commit 2 - Cube Calculation

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // 1.1. What is the difference between a parameter and an argument?
 
+// A parameter is what is listed in the () section of the function definition and is used throughout the 
+// function. The argument is passed in as a value to the function when the function is called later on.
+
 // 1.2. Within a function, what is the difference between return and console.log?
+
+//a returned value is a piece of data that can be used while simply console.logging the result of a function
+// won't be an actionable piece of data
 
 // 1. 3. What are the implications of the ability of a function to return a value?
 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+    return Math.pow(num,3)
 }
 
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
+    if(letter == 'a' | letter == 'e' | letter == 'i' | letter == 'o' | letter == 'u') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // 4. getTwoLengths
