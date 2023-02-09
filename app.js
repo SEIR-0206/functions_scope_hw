f1be224f0598b368a9269b26f74e3c2cf3c6120b
JONPIRES <jonbpires@gmail.com>
2023-02-09 19:23:10 -0300

    Commit 6.1 - Prime Numbers

--- app.js
+++ app.js
@@ -50,12 +50,21 @@ function sumArray(arr) {
     }
     return arrSum;
 }
-console.log(sumArray([1, 2, 3, 4, 5, 6]));
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
     // YOUR CODE HERE
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+       if( num % i === 0){
+        return false
+       }
+      
+    }
+    return true;
 }
+
+console.log(checkPrime(21));
 
 // 6.2 printPrimes
 function printPrimes(num) {
