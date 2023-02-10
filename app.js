c064d03bc0862ecbd445b84eeb1b75148c9a0cac
piquant95 <piquant95@yahoo.com>
2023-02-10 16:16:04 -0500

    Commit 1 - verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // 1.1. What is the difference between a parameter and an argument?
+//Answer: The values that are declared within a function when the function is called are known as an argument. Whereas, the variables that are defined when the function is declared are known as a parameter.
 
 // 1.2. Within a function, what is the difference between return and console.log?
+// Answer: console.log prints the output in the console. Return will return a value when the function is called.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+// (I didn't really understand what this question was asking to be honest so I just googled and pasted the best answer I could find but I still do not quite understand)
+// Answer: When a function returns a value, the value is returned via a return statement to the caller of the function, after being implicitly converted to the return type of the function in which it is defined.
 
 // 2. calculateCube
 function calculateCube(num) {
