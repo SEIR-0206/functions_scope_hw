69e932b9b0145a8833ef4894a73c1cc6ea1afbff
guantanamobosch <luketyson17@gmail.com>
2023-02-10 13:59:01 -0700

    Commit 10 -- finished sum positive array

--- app.js
+++ app.js
@@ -267,8 +267,16 @@ function findNeedle(arr) {
 
 // 10. sumPositive
 function sumPositive(arr) {
-    // YOUR CODE HERE
+    // let sum = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] > 0) {
+    //         sum += arr[i];
+    //     }
+    // }
+    // return sum;
 }
+
+// console.log(sumPositive([1, -4, 7, 20, 12]));
 
 module.exports = {
     calculateCube,
