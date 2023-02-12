a0f20b40cb224203fdbeb14dfd287c5575b42a93
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-02-11 18:42:01 -0700

    Commit 3 - Is a Vowel

--- app.js
+++ app.js
@@ -7,65 +7,74 @@
 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
+  return num * num * num;
 }
+console.log(calculateCube(5));
 
 // 3. isAVowel
-function isAVowel(letter) {
-    // YOUR CODE HERE
+function isAVowel(name) {
+  //name = name.toLowerCase();
+  //if (name == "i" || name == "e" || name == "a" || name == "o" || name =="u") {
+   // return true;
+  //} else {
+   // return false;
+  //}
+  // YOUR CODE HERE
 }
+//console.log(isAVowel("a"));
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 5. sumArray
 function sumArray(arr) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 6.1 checkPrime
 function checkPrime(num) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 6.2 printPrimes
 function printPrimes(num) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // BONUS!
 
 // 8. eulerFibo
 function eulerFibo(num) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 9. findNeedle
 function findNeedle(arr) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 // 10. sumPositive
 function sumPositive(arr) {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 module.exports = {
-    calculateCube,
-    isAVowel,
-    getTwoLengths,
-    sumArray,
-    checkPrime,
-    printPrimes,
-    printLongestWord,
-    eulerFibo,
-    findNeedle,
-    sumPositive
+  calculateCube,
+  isAVowel,
+  getTwoLengths,
+  sumArray,
+  checkPrime,
+  printPrimes,
+  printLongestWord,
+  eulerFibo,
+  findNeedle,
+  sumPositive,
 };
