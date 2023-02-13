c33b6beecae925a6b080ffd09d658deee275d77e
Ali Colak <19acolak99@gmail.com>
2023-02-12 21:01:20 -0600

    Commit 1: Verbal Questions Complete

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // 1.1. What is the difference between a parameter and an argument?
+//Answer: A parameter is a value passed into a function, the argument is the value passed in. For example, function aliColak(parameter){}, when we call, aliColak(arg)
+
 
 // 1.2. Within a function, what is the difference between return and console.log?
+// The return returns the value and exits the loop while the console.log just prints out the inputted value and continues processing. 
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+// The Conclusion of the abilitiy of a function to return a value are: it simplifies & makes code reusable. It also makes the code more flexible because you can include different values whenever you call it.
 
 // 2. calculateCube
 function calculateCube(num) {
